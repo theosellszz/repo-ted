e0f762d468ead1623d5a6df550b87f123533d4f9
theosellszz <ted914063@gmail.com>
2025-05-22 13:53:14 -0400

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Home = () => (
-  <div>
-    <h2>Home Page</h2>
-    <p>Welcome to our store!</p>
-  </div>
-);
+const Home = () => {
+  return (
+    <div className="page">
+      <h1>Welcome to the Resell Shop</h1>
+      <p>We offer premium 1:1 products at unbeatable prices.</p>
+      <Link to="/products" className="btn">View Products</Link>
+    </div>
+  );
+};
 
 export default Home;
