c30e88c1b4613616e2f0f372af30110a1b10b582
theosellszz <ted914063@gmail.com>
2025-05-21 20:01:11 -0400

    Create MainRoutes.jsx

---
+++ src/MainRoutes.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Login from './admin/Login';
+import AdminPanel from './admin/AdminPanel';
+import ProtectedRoute from './admin/ProtectedRoute';
+
+function MainRoutes() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login onLogin={setUser} />} />
+        <Route path="/admin" element={
+          <ProtectedRoute user={user}>
+            <AdminPanel />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </Router>
+  );
+}
+
+export default MainRoutes;
