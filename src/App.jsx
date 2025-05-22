e2eb35a3016d3fdc204b6790bcc5675c90fc17ef
theosellszz <ted914063@gmail.com>
2025-05-22 10:43:33 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import StarryBackground from './components/StarryBackground';
+import { Link } from 'react-router-dom';
+import './styles/global.css';
 
-function App() {
-  return (
-    <div>
-      <StarryBackground />
-      <div style={{ color: 'white', padding: '2rem' }}>
-        <h1>1:1 Resell Website</h1>
-        <p>Products go here...</p>
-        {/* Add your product and checkout components here */}
-      </div>
-    </div>
-  );
-}
+const App = () => (
+  <div className="app-container">
+    <h1>Welcome to The Resell Hub</h1>
+    <nav>
+      <Link to="/products">Products</Link>
+      <Link to="/login">Admin Login</Link>
+    </nav>
+  </div>
+);
 
 export default App;
